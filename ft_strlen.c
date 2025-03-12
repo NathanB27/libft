038b0009ee5a98d809dd28adbe82341f83f1687b
Nathan <nboucher@student.42.fr>
2025-03-12 16:34:45 +0100

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -21,15 +21,3 @@ size_t	ft_strlen(const char *s)
 		i ++;
 	return (i);
 }
-// while s && s[i] car si la chaine passe en param est un
-// pointeur null ca seg fault
-/*
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-		printf("%d\n", ft_strlen(argv[1]));
-	return (0);
-}
-*/
